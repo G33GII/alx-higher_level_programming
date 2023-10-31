7fc98e1bf8010a0e0bc587730e12f8c2914da94b
G33GII <gazasolomon@gmail.com>
2023-11-01 00:35:09 +0100

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(input_str):
+    result = ""
+    for char in input_str:
+        if 'a' <= char <= 'z':
+            # Convert the character to uppercase using ord() and chr()
+            uppercase_char = chr(ord(char) - 32)
+            result += uppercase_char
+        else:
+            result += char
+    print(result)
