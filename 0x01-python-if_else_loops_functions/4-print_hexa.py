2f78f4af7ac98694ff7306c1c29a9e859872614d
G33GII <gazasolomon@gmail.com>
2023-10-31 19:48:20 +0100

    correction

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 for num in range(99):
-    print("{:2d} = 0x{:02x}".format(num, num))
+    if num < 15:
+        print("{:d} = 0x{:x}".format(num, num))
+    else:
+        print("{:d} = 0x{:x}".format(num, num))
+
+
