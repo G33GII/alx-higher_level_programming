cda1b289995223a7b347d988ab14001edf919ae0
G33GII <gazasolomon@gmail.com>
2023-11-01 01:19:27 +0100

    update 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
+    return a ** b
+
+
+"""
+def pow(a, b):
     if a == 0 and b < 0:
         return float('inf')  # Handle division by zero
     if b >= 0:
@@ -12,3 +17,4 @@ def pow(a, b):
         for _ in range(-b):
             result /= a
         return (result)
+"""
