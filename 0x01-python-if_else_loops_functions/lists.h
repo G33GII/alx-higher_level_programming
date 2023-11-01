d74bfb40762e2fc5d1ea3cea0d4717965d108343
_ga_za <gazasolomon@gmail.com>
2023-11-01 14:43:26 +0100

    minor updates

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <stdio.h>
 
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -24,6 +23,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *insert_node(listint_t **head, int number);
 size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
-
 
 #endif /* LISTS_H */
