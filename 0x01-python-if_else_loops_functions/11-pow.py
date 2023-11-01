accd18e3a6b8058f7b2e146492400ae4358b7082
G33GII <gazasolomon@gmail.com>
2023-11-01 01:15:25 +0100

    update

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -6,9 +6,9 @@ def pow(a, b):
         result = 1
         for _ in range(b):
             result *= a
-        return result
+        return (result)
     else:
         result = 1
         for _ in range(-b):
             result /= a
-        return result
+        return (result)
