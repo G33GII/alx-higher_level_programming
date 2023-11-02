6d3aee0ce27f58c1762abef7cd2ad481775c2eab
_ga_za <gazasolomon@gmail.com>
2023-11-02 13:58:03 +0100

    upddate to task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    _dx = len(sys.argv)
 
-_dx = len(sys.argv)
-
-if (_dx == 1):
-    print("{} arguments.".format(_dx - 1))
-else:
-    print("{} arguments:".format(_dx - 1))
-    for i, av in enumerate(sys.argv[1:], 1):
-        print("{}: {}".format(i, av))
+    if (_dx == 1):
+        print("{} arguments.".format(_dx - 1))
+    else:
+        print("{} arguments:".format(_dx - 1))
+        for i, av in enumerate(sys.argv[1:], 1):
+            print("{}: {}".format(i, av))
