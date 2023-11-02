aa320e67cee7392663ca6e6dc6888a3612eb3d0a
_ga_za <gazasolomon@gmail.com>
2023-11-02 14:00:37 +0100

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     _dx = len(sys.argv)
 
     if (_dx == 1):
-        print("{} arguments.".format(_dx - 1))
+        print("{} argument.".format(_dx - 1))
     else:
         print("{} arguments:".format(_dx - 1))
         for i, av in enumerate(sys.argv[1:], 1):
