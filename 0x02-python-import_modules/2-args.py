ca3077494755b72be8e7d153a0dccd2e63317686
_ga_za <gazasolomon@gmail.com>
2023-11-02 13:50:55 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+_dx = len(sys.argv)
+
+if (_dx == 1):
+    print("{} arguments.".format(_dx - 1))
+else:
+    print("{} arguments:".format(_dx - 1))
+    for i, av in enumerate(sys.argv[1:], 1):
+        print("{}: {}".format(i, av))
