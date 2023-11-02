82cc2cf1433f5c189f1dc3532300d05242ab5d74
_ga_za <gazasolomon@gmail.com>
2023-11-02 10:46:07 +0100

    mmade new dir and added the necessary files

---
+++ test/ex.py
@@ -0,0 +1,21 @@
+"""from fibo import fib, fib2
+fib(500)"""
+
+"""import fibo
+fibo.fib(1000)
+fib = fibo.fib
+fib(500)"""
+
+"""from fibo import *
+fib(500)
+print(fib2(500))"""
+
+'''import fibo as _fib
+_fib.fib(500)'''
+
+
+'''from fibo import fib as fibonacci
+from fibo import fib2 as _fibonacci
+
+fibonacci(500)
+print(_fibonacci(800))'''
