1e3a27c075a2bf9868b6f059e62ac2891ba03fa2
_ga_za <gazasolomon@gmail.com>
2023-11-02 18:13:16 +0100

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
-"""
 #!/usr/bin/python3
+"""
 
 if __name__ == "__main__":
     import sys
@@ -14,8 +14,6 @@ if __name__ == "__main__":
     print(_av)
 """
 
-
-#!/usr/bin/python3
 
 def add_arg(argv):
     n = len(argv) - 1
