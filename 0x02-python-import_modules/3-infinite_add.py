5903b7be2eb5d245e61f37d724dd1663f5827a4a
_ga_za <gazasolomon@gmail.com>
2023-11-02 18:08:41 +0100

    update to task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    av = sys.argv
+    _lx = len(av)
+    _av = 0
+
+    for i in range(1, _lx):
+        fx = int(av[i])
+        _av += fx
+    print(_av)
+
+    """
+    #!/usr/bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
+
+    """
