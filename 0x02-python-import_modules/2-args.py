1f998e23f48f7ab7bb58d2b3aac9e4dd79068932
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-03 06:20:22 +0000

    changes to task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-'''
 if __name__ == "__main__":
     import sys
+
     av = sys.argv
-    _dx = len(sys.argv) - 1
+    _dx = len(av) - 1
 
     if (_dx == 0):
         print("{} arguments.".format(_dx))
+
     elif (_dx == 1):
         print("{} argument.".format(_dx))
+        print("{}: {}".format(_dx, av[_dx]))
+
     else:
         print("{} arguments:".format(_dx))
-    for i in range(1, _dx + 1):
-        print("{}: {}".format(i, av[i]))
-
-'''
-if __name__ == "__main__":
-    import sys
-
-    arg = sys.argv
-    size = len(arg) - 1
-
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-
-    elif size == 0:
-        print("{} arguments.".format(size))
-
-    else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
+        for i in range(1, _dx + 1):
+            print("{}: {}".format(i, av[i]))
