268ebc7260c9305256404d9456673023771eeef7
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-03 06:35:33 +0000

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,25 +14,5 @@ if __name__ == "__main__":
             print("{}: {}".format(i, av[i]))
 
     else:
-        print("{} argument.".format(_dx))
+        print("{} argument:".format(_dx))
         print("{}: {}".format(_dx, av[_dx]))
-
-'''
-if __name__ == "__main__":
-    import sys
-
-    arg = sys.argv
-    size = len(arg) - 1
-
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-
-    elif size == 0:
-        print("{} arguments.".format(size))
-
-    else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
-'''
