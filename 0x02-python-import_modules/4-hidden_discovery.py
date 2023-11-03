fbbeec02dcc415094fa856d5e317e0f4d862ee3b
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-03 06:56:41 +0000

    updated task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""if __name__ == "__main__":
-    import hidden_4
-
-    for name in sorted(dir(hidden_4)):
-        if name[:2] != '__':
-            print("{}".format(name))"""
 
 if __name__ == "__main__":
     import hidden_4
 
     for name in sorted(dir(hidden_4)):
-        if not name.startswith('__'):
+        if not name.startswith('__'):   #  OR (if name[:2] != '__':)
             print(name)
