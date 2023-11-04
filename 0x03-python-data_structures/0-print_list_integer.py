7642ca2e5a068dfd29af8a89e54585e0764f5c31
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-04 09:44:43 +0000

    modified:   0x03-python-data_structures/0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,7 @@ def print_list_integer(my_list=[]):
     for num in range(len(my_list)):
         print("{:d}".format(my_list[num]))
 
+
 """#!/usr/bin/python3
 def print_list_integer(my_list=[]):
     str = "{}"
