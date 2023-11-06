d846da37a9c91f952e59f5f119840d39b8b23328
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 10:21:13 +0000

    task 3

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -2,7 +2,7 @@
 
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 9, 8, 5, 7, 8]
 print_reversed_list_integer(my_list)
 
 """Prototype: def print_reversed_list_integer(my_list=[]):
