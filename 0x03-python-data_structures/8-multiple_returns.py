f975a335d91bf0678cba7f3a20b930f846822f1d
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 19:48:19 +0000

    task 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    """Function that: """
+    """Function that: returns a tuple: length & first character of a str"""
     return len(sentence), None if len(sentence) == 0 else sentence[0]
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """Function that finds the biggest integer of a list"""
+    _rx = 0
+    for x in my_list:
+        _rx = x if x > _rx else _rx
+    return _rx
