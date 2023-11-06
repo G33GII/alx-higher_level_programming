2a7357f5dfe2bd447e1d35ce32c54c1b38cf5989
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 09:47:59 +0000

    working on task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
 
+def element_at(my_list, idx):
+    """Function that replaces the element at 'idx' if it's a valid index."""
     dx = len(my_list)
 
-    if idx < 0:
-        return None
-    if idx > dx:
+    if idx < 0 or idx >= dx:
         return None
     return my_list[idx]
--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -2,13 +2,13 @@
 element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
-IDX = 3
+IDX = 5
 print("Element at index {:d} is {}".format(IDX, element_at(my_list, IDX)))
 
-    """
+"""
 Prototype: def element_at(my_list, idx):
 If idx is negative, the function should return None
 If idx is out of range (> of number of element in my_list), the function should return None
 You are not allowed to import any module
 You are not allowed to use try/except
-    """
+"""
