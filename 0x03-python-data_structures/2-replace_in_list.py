59529b3490e98a3c69d6026d6f2fc19c292c4351
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 10:01:01 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 def replace_in_list(my_list, idx, element):
     """Function that replaces an element of a list at a specific position."""
     if idx < 0 or idx >= len(my_list):
-        return None  # Index is out of bounds
+        return my_list  # Index is out of bounds
     my_list[idx] = element
     return my_list
