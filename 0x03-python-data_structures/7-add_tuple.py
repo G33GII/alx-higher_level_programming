4761679f4b1151584bb95fec7dce87e960469a15
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 14:15:07 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,14 +6,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
         lx_a = len(tuple_a)
         lx_b = len(tuple_b)
-        lx_ = max(lx_a, lx_b)  # Use the maximum length
+        lx_ = 2  # Use the maximum length
 
         nw_ = []  # Initialize a list to store the sums
 
         for i in range(lx_):
             _a = tuple_a[i] if i < lx_a else 0  # Use 0 for missing elements
             _b = tuple_b[i] if i < lx_b else 0  # Use 0 for missing elements
-            nw = _a + _b
+            nw = int(_a) + int(_b)
             nw_.append(nw)
 
         return tuple(nw_)  # Return the result as a tuple
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -2,13 +2,13 @@
 
 add_tuple = __import__('7-add_tuple').add_tuple
 
-tuple_a = (1, 89)
+tuple_a = (1, 89, 23)
 tuple_b = (88, 11)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
  
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
 
 """Write a function that adds 2 tuples.
 
