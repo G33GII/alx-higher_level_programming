728878a1f4221bb496c5091b3d364d604a8370c9
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 14:08:12 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,16 +3,17 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     """Function that adds two tuples element-wise and returns a new tuple."""
 
-    lx_a = len(tuple_a)
-    lx_b = len(tuple_b)
-    lx_ = max(lx_a, lx_b)  # Use the maximum length
+    if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
+        lx_a = len(tuple_a)
+        lx_b = len(tuple_b)
+        lx_ = max(lx_a, lx_b)  # Use the maximum length
 
-    nw_ = []  # Initialize a list to store the sums
+        nw_ = []  # Initialize a list to store the sums
 
-    for i in range(lx_):
-        _a = tuple_a[i] if i < lx_a else 0  # Use 0 for missing elements
-        _b = tuple_b[i] if i < lx_b else 0  # Use 0 for missing elements
-        nw = _a + _b
-        nw_.append(nw)
+        for i in range(lx_):
+            _a = tuple_a[i] if i < lx_a else 0  # Use 0 for missing elements
+            _b = tuple_b[i] if i < lx_b else 0  # Use 0 for missing elements
+            nw = _a + _b
+            nw_.append(nw)
 
-    return tuple(nw_)  # Return the result as a tuple
+        return tuple(nw_)  # Return the result as a tuple
