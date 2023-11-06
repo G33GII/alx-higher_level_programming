b6f1268f137ab79b3199242209cef6326bb432c2
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-06 21:08:20 +0000

    update to taask 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,13 +3,13 @@
 def delete_at(my_list=[], idx=0):
     """ function that deletes the item at a specific position in a list """
     _dx = len(my_list)
-    _nw = []
 
     if my_list and idx > -1 and idx < _dx:
-        _nw = [my_list[x] for x in range(_dx) if x != idx]
+        # my_list = [my_list[x] for x in range(_dx) if x != idx]
         """for i in range(_dx):
             if i != idx:
                 _nw.append(my_list[i])"""
-        return (_nw)
+        del my_list[idx]
+        return (my_list)
 
     return (my_list)
