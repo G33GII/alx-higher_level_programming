efc44acc82ee85850be5419c87976994b6750a20
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-08 20:32:17 +0000

    list comprehnsion task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+def uniq_add(my_list=[]):
+    """
+    Function that adds all unique integers in a list (only once for each integer).
+
+    Args:
+        my_list (list, optional): The input list. Defaults to [].
+    """
+    unique_set = set(my_list)
+    total = 0
+
+    for itm in unique_set:
+        total += itm
+
+    return total
