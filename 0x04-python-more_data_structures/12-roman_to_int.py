00ec5ac1ebfbc829de6ecfaeed5d094e347c5518
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-09 00:48:48 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 def roman_to_int(roman_string):
 
-    if not roman_string or type(roman_string) != str:
+    if not roman_string or type(roman_string) is not str:
         return 0
 
     roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -17,7 +17,8 @@ def roman_to_int(roman_string):
 
     return roman_n
 
-    """roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    """
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_n = 0
 
     prev_value = 0
