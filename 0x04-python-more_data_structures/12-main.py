e98a15ef5b73a40156410b1978d0e03f617ca0f7
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-09 00:42:24 +0000

    Roman Numerial

---
+++ 0x04-python-more_data_structures/12-main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+"""Technical interview preparation:
+
+You are not allowed to google anything
+Whiteboard first
+Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.
+
+You can assume the number will be between 1 to 3999.
+def roman_to_int(roman_string) must return an integer
+If the roman_string is not a string or None, return 0
+"""
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    """if not roman_string or type(roman_string) != str:
+        return 0
+
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
+        else:
+            roman_n += roman_d[roman_string[j]]
+
+    return roman_n"""
+
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+
+    prev_value = 0
+    for c in roman_string[::-1]:  # Reverse the string for easy subtraction logic
+        value = roman_d.get(c, 0)
+
+        if value >= prev_value:
+            roman_n += value
+        else:
+            roman_n -= value
+
+        prev_value = value
+
+    return roman_n
