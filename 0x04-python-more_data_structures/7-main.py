6e5e8f7d40512ff70545e4fa17edc7a14c76970b
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-09 00:00:04 +0000

    update to task 7

---
+++ 0x04-python-more_data_structures/7-main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+update_dictionary = __import__('7-update_dictionary').update_dictionary
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+
+"""Write a function that replaces or adds key/value in a dictionary.
+
+Prototype: def update_dictionary(a_dictionary, key, value):
+key argument will be always a string
+value argument will be any type
+If a key exists in the dictionary, the value will be replaced
+If a key doesn’t exist in the dictionary, it will be created
+You are not allowed to import any module
+"""
