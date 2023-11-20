93eb2475e92feb2970d52bc6127e4054cb131fc5
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-20 22:10:21 +0100

    update {:d}".format()

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     z = 0
     for n in range(x):
         try:
-            print(int(my_list[n]), end='')
+            print("{:d}".format(my_list[n]), end='')
             z += 1
         except (IndexError, ValueError, TypeError):
             continue
