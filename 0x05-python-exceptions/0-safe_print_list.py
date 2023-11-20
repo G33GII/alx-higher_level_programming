6fd0ccb830920419e4e70cf4567696f870d4b7ff
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-20 22:38:07 +0100

    update task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     z = 0
     for n in range(x):
         try:
             print("{:d}".format(my_list[n]), end='')
             z += 1
-        except (IndexError, ValueError, TypeError, AttributeError):
+        except:
             pass
     print()
     return z
