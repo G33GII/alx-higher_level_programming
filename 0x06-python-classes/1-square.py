b988112ed85df5e0879dda5c71a5e6e354ed4fec
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-21 15:46:17 +0100

    task 1 update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a given size.
+
+        Args:
+            size: Size of the square (private instance attribute).
+        """
+        self.__size = size
+
