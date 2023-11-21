c69a9b4f6f94ab1909a93bd729cb0673870ab687
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-21 20:35:01 +0100

    update task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Square:
+    """This class represents a square.
+
+        Args:
+            size (int): Size of the square (private instance attribute).
+                Defaults to 0 if not specified.
+    """
+    def __init__(self, size=0):
+           """Initialize a square with an optional size.
+
+        Args:
+            size (int): Size of the square (private instance attribute).
+                Defaults to 0 if not specified.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
