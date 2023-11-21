8f6f58fea5d7ca21acafea2e86ba26c8ee8bb947
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-21 20:47:03 +0100

    update tasak 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Defines a CLASS """
+
+class Square:
+    """
+    Class that defines properties of a square (based on 2-square.py).
+
+    Attributes:
+        __size: Size of a square (1 side).
+    """
+
+    def __init__(self, size=0):
+        """Creates new instances of a square.
+
+        Args:
+            size (int): Size of the square (1 side). Defaults to 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            int: The current square area.
+        """
+        return self.__size ** 2
