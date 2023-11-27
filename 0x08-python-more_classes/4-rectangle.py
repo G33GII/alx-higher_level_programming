78d7907e03abd25fe04c0d33c21639a53b1f261f
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-27 22:07:02 +0100

    update task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -68,11 +68,9 @@ class Rectangle:
             raise ValueError("Height must be >= 0")
         self.__height = value
 
-
     def area(self):
         """Calculate the area of the rectangle."""
         return self.__width * self.__height
-
 
     def perimeter(self):
         """Calculate the perimeter of the rectangle."""
