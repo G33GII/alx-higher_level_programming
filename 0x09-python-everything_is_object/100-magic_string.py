44f9d7a79a3feed4b56f91a5dc9461eabe2f61d7
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-28 23:31:37 +0100

    update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""A Magic string method"""
+
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
