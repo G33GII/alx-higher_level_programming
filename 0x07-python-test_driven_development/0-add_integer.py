26f03d4d1edf721da4234ba575c40b7541d4278f
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-28 23:45:32 +0100

    task 0 doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-""" """
+"""Module for adding two integers."""
+
 
 def add_integer(a, b=98):
-    """Function """
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    elif type(b) not in [int, float]:
-        raise TypeError("a must be an integer")
-    return (a + b)
+    """Adds two integers.
 
+    Args:
+        first_integer: The first integer to be added.
+        second_integer: The second integer to be added, defaults to 98.
+
+    Raises:
+        TypeError: If either a or b are not int or float.
+
+    Returns:
+        The sum of the two integers.
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
