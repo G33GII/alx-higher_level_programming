410a57901a4a2f9363cd263e01ed1dbab2a97b07
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-29 20:29:27 +0100

    doctest update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,9 +20,10 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
         raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
 
-    return int(a) + int(b)
-
+    return a + b
 
 if __name__ == "__main__":
     import doctest
