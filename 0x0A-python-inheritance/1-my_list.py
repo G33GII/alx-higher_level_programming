35404961b78d5e23e458d74804b4a1e221daffe6
G33GII <gazasolomon@gmail.com>
2023-12-04 23:14:28 +0100

    update to Mylist print_sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,8 +8,3 @@ class MyList(list):
     def print_sorted(self):
         """Print the list in sorted order."""
         print(sorted(self))
-
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile("tests/1-my_list.txt")
