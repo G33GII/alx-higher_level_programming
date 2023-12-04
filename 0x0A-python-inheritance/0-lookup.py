af6af016da06a59f3de038248d64b9320935940e
G33GII <gazasolomon@gmail.com>
2023-12-04 21:05:46 +0100

    update t6o lookup dir()

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,7 +4,8 @@ list of available attributes and methods of an object"""
 
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object
+    """function that returns
+    the list of available attributes and methods of an object
 
     Args:
         obj: Object to be examined.
