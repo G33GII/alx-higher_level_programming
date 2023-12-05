01fb950affc81d7073d738e4b7b3132eb5d857ee
G33GII <gazasolomon@gmail.com>
2023-12-06 00:38:04 +0100

    file number 9 (task)

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""class: Student that defines a student"""
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Create a JSON-like representation of the Student object.
+
+        Returns:
+            dict: A dictionary representation of the Student object.
+        """
+        return self.__dict__
