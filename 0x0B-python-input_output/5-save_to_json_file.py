cb82dc5f533413e68789cb23324a626877548f60
G33GII <gazasolomon@gmail.com>
2023-12-05 19:58:14 +0100

    modified:  5-save_to_json_file.py, 5-save_to_json_file. deleted: file_append.txt, my_first_file.txt

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,7 +6,6 @@ def save_to_json_file(my_obj, filename):
     """from_json_string: writes an Object to a txt,
                         using a JSON representation"""
     json = __import__("json").dumps
-    __r = json(my_obj)
 
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(__r)
+        f.write(json(my_obj))
