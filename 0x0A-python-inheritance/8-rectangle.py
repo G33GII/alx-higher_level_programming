3d30bf334952092a6fc40ef34dccb6eb58af9339
G33GII <gazasolomon@gmail.com>
2023-12-06 19:11:32 +0100

    update to subclass file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python3
 """A Class"""
-
-
-class BaseGeometry(object):
-    """BaseGeometry"""
-
-    def area(self):
-        """Method:
-            raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name(str):
-            value(int):
-        Raises:
-            TypeError:
-            ValueError:
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -34,10 +11,10 @@ class Rectangle(BaseGeometry):
         using a method in the parent to validate width & height
 
         Args:
-            width(int): mostr be an integer
-            height(int): mostr be an integer
+            width(int): most be an integer
+            height(int): most be an integer
         """
         self.integer_validator("height", height)
-        self.__height = height
         self.integer_validator("width", width)
+        self.__height = height
         self.__width = width
