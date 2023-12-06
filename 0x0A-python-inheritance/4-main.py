c75f88639eae61bba5cb7461f5f18c961489c3b0
G33GII <gazasolomon@gmail.com>
2023-12-06 17:03:25 +0100

    update

--- 0x0A-python-inheritance/4-main.py
+++ 0x0A-python-inheritance/4-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 inherits_from = __import__('4-inherits_from').inherits_from
 
-a = True
+a = 1
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):
     print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
+if inherits_from(a, int):
     print("{} inherited from class {}".format(a, object.__name__))
