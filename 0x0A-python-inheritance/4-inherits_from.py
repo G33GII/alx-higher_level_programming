4ec3422fef771159ce4d8b3686bd1f0af71088fc
G33GII <gazasolomon@gmail.com>
2023-12-06 16:54:02 +0100

    update to task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A function"""
+
+
+def inherits_from(obj, a_class):
+    """Function: That checks direct or indirect inheritance from another class
+
+    Args:
+        obj: An object
+        a_class: A class
+
+    Returns:
+        returns True if the object is an instance of a class
+        that inherited (directly or indirectly) from the specified class
+    """
+    if issubclass(type(obj), a_class):
+        return True
+    return False
