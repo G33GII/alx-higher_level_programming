4c5aaaea7ac6f1f6f53ab1fef3697099051d3965
G33GII <gazasolomon@gmail.com>
2023-12-06 20:02:42 +0100

    update to path on task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,54 +1,6 @@
 #!/usr/bin/python3
 """A Class"""
-
-
-class BaseGeometry(object):
-    """BaseGeometry"""
-
-    def area(self):
-        """Method:
-            raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name(str): The name of the parameter.
-            value(int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """Reangle Class"""
-
-    def __init__(self, width, height):
-        """Initialises heighth and width.
-        using a method in the parent to validate width & height
-
-        Args:
-            width(int): most be an integer
-            height(int): most be an integer
-        """
-        self.integer_validator("height", height)
-        self.integer_validator("width", width)
-        self.__height = height
-        self.__width = width
-
-    def area(self):
-        """Area: different from the parent area method"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """___str___: for print(obj)"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
