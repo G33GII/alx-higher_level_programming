f5ea3ac88e5622e3f670456bc10cc8c8cf26dd50
G33GII <gazasolomon@gmail.com>
2023-12-12 08:43:35 +0100

    documentation on rectangle file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" A Class:
-        class Rectangle that inherits from Base.
-"""
+""" A Class: class Rectangle that inherits from Base."""
 from base import Base
 
 
@@ -57,6 +55,7 @@ class Rectangle(Base):
 
     """*******************************************"""
     def __str__(self):
+        """__str__: print(obj)"""
         return (
             "[Rectangle] ({}) {}/{} - {}/{}"
             .format(self.id, self.x, self.y, self.width, self.height)
