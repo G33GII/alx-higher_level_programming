14898de47990cb08bfbd32bd8355c73e4fc71ff3
G33GII <gazasolomon@gmail.com>
2023-12-12 20:54:27 +0100

    dealing with json on python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ A Class:
         This class will be the “base” of all other classes in this project.
 """
+import json
 
 
 class Base(object):
@@ -25,3 +26,10 @@ class Base(object):
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """to_json_string: returns the JSON
+        string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
