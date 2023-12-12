1ea9edc055b5dfc58ffc6daf82a49f2c50b2be54
G33GII <gazasolomon@gmail.com>
2023-12-12 10:14:29 +0100

    made square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A Class: This class inherits from Rectangle."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class: inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Rectangle Class: Constructor.
+
+        Args:
+            size (int): height and width == size.
+            id (int):  Defaults to None.
+            x (int): X axis.
+            y (int): y axis.
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """__str__: print(obj)"""
+        return (
+            "[Square] ({}) {}/{} - {}/{}"
+            .format(self.id, self.x, self.y, self.width, self.width)
+            )
