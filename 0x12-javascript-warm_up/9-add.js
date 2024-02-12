51d9cdbfbad67206b22a3895b68774f455818020
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 23:40:34 +0000

    Tue, Feb 13, 2024, 12:40 AM +01:00

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+
+#!/usr/bin / node
+
+function add(a, b) {
+    return a + b;
+}
+
+const firstInteger = parseInt(process.argv[2]);
+const secondInteger = parseInt(process.argv[3]);
+
+if (!isNaN(firstInteger) && !isNaN(secondInteger)) {
+    console.log(add(firstInteger, secondInteger));
+} else {
+    console.log("Please provide two valid integers");
+}
