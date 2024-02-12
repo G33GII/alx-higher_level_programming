d0aed82932af9de91c2fa4bd5006dba40dd16359
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 23:58:21 +0000

    Tue, Feb 13, 2024, 12:58 AM +01:00

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+const args = process.argv.slice(2).map(Number);
+
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  const replacedArgs = args.map(num => num === 12 ? 89 : num);
+
+  replacedArgs.sort((a, b) => b - a);
+
+  const secondLargest = replacedArgs[1];
+
+  console.log(secondLargest);
+}
