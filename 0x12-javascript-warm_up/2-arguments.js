349ae3c7db9c8ad80069edd54b31372e10fb41b9
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 23:16:41 +0100

    using argv in javascritps

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,9 @@
 const numOfArgs = process.argv.length - 2;
 
 if (numOfArgs === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (numOfArgs === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Argumemnts fount');
+  console.log('Arguments found');
 }
-
