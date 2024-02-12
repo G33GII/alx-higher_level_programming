eaab58742184bb87701d0240f2bdcd8d59e17c29
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 22:14:40 +0000

    Mon, Feb 12, 2024, 11:14 PM +01:00

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+const numOfArgs = process.argv.length - 2;
+
+if (numOfArgs === 0) {
+    console.log('No argument');
+} else if (numOfAgrs === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Argumemnts fount');
+}
+
