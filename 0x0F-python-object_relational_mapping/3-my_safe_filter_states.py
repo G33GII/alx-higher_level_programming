dd2450c9a2da6de41d84546f530abd1272f5c776
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-16 20:51:34 +0100

    task 3 safe from  injection attack

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+import sys
+import MySQLdb
+
+
+def search_states(username, password, database, state_name):
+    try:
+        # Connect to MySQL server
+        connection = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+        cursor = connection.cursor()
+
+        # Execute safe query to retrieve states matching the state_name
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+        states = cursor.fetchall()
+
+        # Display states
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close connection
+        if connection:
+            connection.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    search_states(username, password, database, state_name)
