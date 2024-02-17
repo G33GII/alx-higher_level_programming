e411e8bd607991d69fb1efd0ecd51f37246a5251
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-17 16:31:14 +0100

    Update to task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,48 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+
 import sys
+import MySQLdb
+
+
+def list_cities(username, password, database):
+    try:
+        # Connect to MySQL server
+        connection = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+        cursor = connection.cursor()
+
+        # Execute query to retrieve cities
+        query = "SELECT * FROM cities ORDER BY id ASC"
+        cursor.execute(query)
+        cities = cursor.fetchall()
+
+        # Display cities
+        tmp = list(row[0] for row in cities)
+        print(*tmp, sep=", ")
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close connection
+        if connection:
+            connection.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
-    rows = cur.fetchall()
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
 
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    cur.close()
-    db.close()
+    list_cities(username, password, database)
