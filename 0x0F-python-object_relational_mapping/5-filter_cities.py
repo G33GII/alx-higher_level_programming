85372be3ba4c03ab0ee1f33e715f8c27085e38a8
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-17 16:46:37 +0100

    Upgraded

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,21 +20,18 @@ if __name__ == "__main__":
     db_cursor = db_connection.cursor()
 
     # Define the SQL query to retrieve cities for the given state
-    sql_query = """SELECT cities.name
+    # Execute the SQL query with parameterized input for the state name
+    db_cursor.execute("""SELECT cities.name
                    FROM cities
                    INNER JOIN states ON states.id=cities.state_id
-                   WHERE states.name=%s"""
-
-    # Execute the SQL query with parameterized input for the state name
-    state_name = sys.argv[4]
-    db_cursor.execute(sql_query, (state_name,))
+                   WHERE states.name=%s""", (sys.argv[4],))
 
     # Fetch all rows from the result set
     rows = db_cursor.fetchall()
 
     # Extract city names from fetched rows and create a list
     city_names = list(row[0] for row in rows)
-    
+
     # Print the city names separated by commas
     print(*city_names, sep=", ", end=".\n")
 
