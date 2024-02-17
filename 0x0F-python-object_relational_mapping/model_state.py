9bca9bbc8cd847d09e429c72b1b54eac01c5503e
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-17 18:36:08 +0100

    update Bug fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,6 @@ definition of a State and an instance Base = declarative_base()
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
@@ -17,5 +15,4 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-
     name = Column(String(128), nullable=False)
