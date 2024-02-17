578098a7a7966f3e003344a52c24beca5563c370
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-17 16:48:43 +0100

    update....

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,18 @@
 #!/usr/bin/python3
-"""
-Lists all cities from the database hbtn_0e_0_usa for a given state.
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Connect to MySQL server using command-line arguments
-    db_connection = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-
-    # Create a cursor object to execute SQL queries
-    db_cursor = db_connection.cursor()
-
-    # Define the SQL query to retrieve cities for the given state
-    # Execute the SQL query with parameterized input for the state name
-    db_cursor.execute("""SELECT cities.name
-                   FROM cities
-                   INNER JOIN states ON states.id=cities.state_id
-                   WHERE states.name=%s""", (sys.argv[4],))
-
-    # Fetch all rows from the result set
-    rows = db_cursor.fetchall()
-
-    # Extract city names from fetched rows and create a list
-    city_names = list(row[0] for row in rows)
-
-    # Print the city names separated by commas
-    print(*city_names, sep=", ", end=".\n")
-
-    # Close cursor and database connection
-    db_cursor.close()
-    db_connection.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
