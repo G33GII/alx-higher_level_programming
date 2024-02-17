2e73834d126112d5a5f20033895ab985fe3bd9f5
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-17 14:18:19 +0100

    Update Fixing Bug

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,46 +1,18 @@
 #!/usr/bin/python3
-"""A script that takes in the name of a state as an argument and
-lists all cities of that state, using the database hbtn_0e_4_usa"""
-
-
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-
-    # Extract MySQL connection information from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host='localhost',
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
-    cursor = db.cursor()
-
-    # Prepare SQL query with parameterized input to avoid SQL injection
-    query =
-
-    # Execute SQL query with parameterized input
-    cursor.execute("""SELECT cities.name FROM
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (state_name,))
-
-    # Fetch all rows from the result set
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
-    if 'cursor' in locals():
-        cursor.close()
-    if 'db' in locals():
-        db.close()
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
