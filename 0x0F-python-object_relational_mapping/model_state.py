71c0e8745977518015d33516f95a7099cde50a50
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-18 14:46:01 +0100

    Testing

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,6 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """Base Class"""
     __tablename__ = 'states'
     id = Column(Integer,  unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
