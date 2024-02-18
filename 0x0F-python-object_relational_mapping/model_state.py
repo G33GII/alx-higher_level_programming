ef0e7a4344074b202b772ee8eb61fb96134c470c
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-18 13:35:56 +0000

    Sun, Feb 18, 2024, 2:35 PM +01:00

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,5 +13,5 @@ Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
