58031af0c2b128493845a2bfbd9d0034e872cba4
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-22 12:48:39 +0000

    Fri, Mar 22, 2024, 1:48 PM +01:00

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines the City class"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class to represent cities in a database"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    def __repr__(self):
+        return "<City(id='%s', name='%s', state_id='%s')>" % (self.id, self.name, self.state_id)
