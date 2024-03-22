5b5459f1fdadf523ce3e41a9d0efeea777f52cab
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-22 13:25:00 +0100

    update to task 12 of ORM

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 
 def change_state_name(username, password, db_name):
     """Change the name of the State where id = 2 to New Mexico"""
-    
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, db_name),
                            pool_pre_ping=True)
@@ -21,9 +21,6 @@ def change_state_name(username, password, db_name):
     if state_to_change:
         state_to_change.name = 'New Mexico'
         session.commit()
-        print("State name changed successfully.")
-    else:
-        print("State with id=2 not found.")
 
 if __name__ == "__main__":=
     if len(sys.argv) != 4:
