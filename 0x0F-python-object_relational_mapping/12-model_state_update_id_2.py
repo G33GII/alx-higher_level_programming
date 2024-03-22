8dc2f0ef60ca77162d1e584031244429fe84e4c8
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-22 13:38:42 +0100

    rearranging task 12 ORM

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,11 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def change_state_name(username, password, db_name):
+if __name__ == "__main__":
     """Change the name of the State where id = 2 to New Mexico"""
 
+    username, password, db_name = sys.argv[1:]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name))
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
@@ -20,7 +22,3 @@ def change_state_name(username, password, db_name):
     if state_to_change:
         state_to_change.name = 'New Mexico'
         session.commit()
-
-if __name__ == "__main__":=
-    username, password, db_name = sys.argv[1:]
-    change_state_name(username, password, db_name)
