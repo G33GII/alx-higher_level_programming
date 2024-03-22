328c37c04237443cc7bec7a98b848d1d811921eb
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-22 12:44:53 +0000

    Fri, Mar 22, 2024, 1:44 PM +01:00

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Script to delete State objects with a name containing the letter 'a' from the database"""
+"""Script to delete State objects with a name
+containing the letter 'a' from the database"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
 
 def delete_states_with_a(username, password, db_name):
     """Delete State objects with a name containing the letter 'a'"""
@@ -17,12 +19,12 @@ def delete_states_with_a(username, password, db_name):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(State.name.like
+                                                   ('%a%')).all()
     if states_to_delete:
         for state in states_to_delete:
             session.delete(state)
         session.commit()
-
 
 if __name__ == "__main__":
     username, password, db_name = sys.argv[1:]
