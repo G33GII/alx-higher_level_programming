dd6d3a6175c992c1bb95fbcef84c12c3e2c46107
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-22 12:20:35 +0000

    Fri, Mar 22, 2024, 1:20 PM +01:00

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,6 +8,7 @@ from model_state import Base, State
 
 def change_state_name(username, password, db_name):
     """Change the name of the State where id = 2 to New Mexico"""
+    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, db_name),
                            pool_pre_ping=True)
