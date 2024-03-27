875836447132e2c32fe8465f9e5a983bdb333109
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-27 22:59:17 +0100

    making it just three lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-# Check if URL argument is provided
-
-# Get the URL from the command line argument
-url="$1"
-
-# Send a request to the URL using curl and store the response body in a temporary file
-response=$(curl -sS --compressed -o /tmp/response_body.txt -w "%{size_download}" "$url")
-
-
-
-# Display the size of the response body in bytes
-echo "Size of the response body: $response bytes"
-
-# Clean up temporary file
-rm -f /tmp/response_body.txt
+# bash sript
+response=$(curl -sS --compressed -o /tmp/response_body.txt -w "%{size_download}" "$1")
