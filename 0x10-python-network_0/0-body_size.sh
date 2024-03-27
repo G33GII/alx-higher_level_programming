bbd23a16d6dd58d6a08fbed2dd36d843aade889f
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-27 21:52:02 +0000

    Wed, Mar 27, 2024, 10:52 PM +01:00

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 # Check if URL argument is provided
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
 # Get the URL from the command line argument
 url="$1"
 
 # Send a request to the URL using curl and store the response body in a temporary file
 response=$(curl -sS --compressed -o /tmp/response_body.txt -w "%{size_download}" "$url")
 
-# Check if curl command was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to fetch URL: $url"
-    exit 1
-fi
+
 
 # Display the size of the response body in bytes
 echo "Size of the response body: $response bytes"
