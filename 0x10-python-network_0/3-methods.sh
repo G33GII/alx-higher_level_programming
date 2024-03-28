b3778956e1270dcd69fb73e8a35211ac57e6a2f6
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 13:29:11 +0100

    modified:   0x10-python-network_0/3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Check if URL argument is provided
-curl -sS --head "$1")" | grep -i '^Allow:' | sed 's/Allow: //i'
-curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
+curl -sS --head "$1" | grep -i '^Allow:' | sed 's/Allow: //i'
