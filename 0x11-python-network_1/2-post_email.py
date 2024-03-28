a237f19f0d400cbfbf666e1339ed40b679faa3a9
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 20:14:20 +0000

    Thu, Mar 28, 2024, 9:14 PM +01:00

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(combo) as response:
         # Read and decode the body of the response in utf-8
         body = response.read().decode('utf-8')
-        if 'X-Request-Id' in response.headers:
-            request_id = response.headers['X-Request-Id']
-            print(request_id)
+        if 'email' in boby:
+            
+            print(f"Your email is: {request_id}")
