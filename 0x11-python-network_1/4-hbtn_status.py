8d2f52945e2a621bdc5af816fbb5d932df026ca3
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 20:53:28 +0000

    Thu, Mar 28, 2024, 9:53 PM +01:00

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Display the body of the response with tabulation before each line
+    print("- body response:")
+    for line in response.text.split('\n'):
+        print(f"\t- {line}")
