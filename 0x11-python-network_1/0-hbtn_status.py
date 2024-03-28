1dc5e9afbf828a6f791807bab2c7f46f2203d49e
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 19:22:47 +0000

    Thu, Mar 28, 2024, 8:22 PM +01:00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""Python Script"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status'
-                                ) as response:
-
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content_type = type(response.read())  # Getting the type of content
         response_content = response.read()    # Reading the response content
         utf8_content = response_content.decode(
             'utf-8')  # Decoding content to UTF-8
 
         print("Body response:")
-        # Since we are dealing with bytes
-        print(f"\t- type: {content_type}")
-        print(f"\t- content: {response_content}")
-        print(f"\t- utf8 content: {utf8_content}")
+        print(f"    - type: {content_type}")
+        print(f"    - content: {response_content}")
+        print(f"    - utf8 content: {utf8_content}")
