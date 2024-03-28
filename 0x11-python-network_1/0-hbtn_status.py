90574475567138680b4eacc9641079960312abd3
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 19:31:37 +0000

    Thu, Mar 28, 2024, 8:31 PM +01:00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status'
+                                ) as response:
         # Read the response content only once
         response_content = response.read()
         bytes = type(response_content)
@@ -13,6 +14,6 @@ if __name__ == "__main__":
         utf8_content = response_content.decode('utf-8')
 
         print("Body response:")
-        print(f"    - type: {bytes}")  # Since we are dealing with bytes
-        print(f"    - content: {response_content}")
-        print(f"    - utf8 content: {utf8_content}")
+        print(f"\t- type: {bytes}")  # Since we are dealing with bytes
+        print(f"\t- content: {response_content}")
+        print(f"\t- utf8 content: {utf8_content}")
