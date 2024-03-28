353e268f7a7ae1e5f876c2050c5d69c94441f13d
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 21:44:28 +0000

    Thu, Mar 28, 2024, 10:44 PM +01:00

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Set the default value for the letter parameter
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    # Send a POST request to the specified URL with the letter as a parameter
+    response = requests.post(
+        'http://0.0.0.0:5000/search_user', data={'q': letter})
+
+    try:
+        # Parse the response as JSON
+        data = response.json()
+
+        # Check if the JSON response is not empty and properly formatted
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
