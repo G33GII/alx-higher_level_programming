0ae0606a97210a0ff310d6426dbb38a92c6ea860
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 21:18:55 +0000

    Thu, Mar 28, 2024, 10:18 PM +01:00

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Get URL and email from command-line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Data to be sent in the POST request
+    data = {'email': email}
+
+    # Send a POST request to the URL with the email as a parameter
+    response = requests.post(url, data=data)
+
+    # Display the body of the response
+    print(response.text)
