245207b8fad3c26bcfe254d53d4cbef449237810
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 22:37:55 +0100

    minor updates

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,8 +3,10 @@
 Python script that takes in a URL,
 sends a request to the URL and displays the body of the response.
 """
+
 import requests
 import sys
+
 
 if __name__ == "__main__":
     # Get URL from command-line arguments
