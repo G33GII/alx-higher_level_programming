73bc0a8a4bddd420c070c678366719b50093cb7c
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 19:37:07 +0000

    Thu, Mar 28, 2024, 8:37 PM +01:00

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,13 +5,8 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    # Check if the script has been provided with a URL argument
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
 
     url = sys.argv[1]
-
     # Send a request to the URL
     with urllib.request.urlopen(url) as response:
         # Check if the 'X-Request-Id' header is present in the response
