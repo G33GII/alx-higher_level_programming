c3de3cb4d5bc2b5eea7c5759777cfe516c617686
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 20:06:13 +0100

    learning to use urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Python Script"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status'
+                                ) as response:
         content_type = type(response.read())  # Getting the type of content
         response_content = response.read()    # Reading the response content
         utf8_content = response_content.decode(
