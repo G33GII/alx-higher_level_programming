7b5b01214b6fadcbf16942fd5e84ce9707b71ab4
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-03-28 19:00:27 +0000

    Thu, Mar 28, 2024, 8:00 PM +01:00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-
 import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        for h in html:
-            print(h)
+        html = response.read().decode('utf-8')
+        print(html)
