ac950ea671aafd631d5648601f053de088db3b50
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-24 00:21:27 +0100

    semi standard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,6 @@ const request = require('request');
 // Import the 'request' module.
 
 request.get(process.argv[2])
-    .on('response', function(response) {
-
-        console.log(`code: ${response.statusCode}`);
-    });
-
+  .on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
+  });
