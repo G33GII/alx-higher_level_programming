d160557b07249ef16ed173fab338cd68a8dcfb34
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-24 00:19:43 +0100

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+// Import the 'request' module.
+
+request.get(process.argv[2])
+    .on('response', function(response) {
+
+        console.log(`code: ${response.statusCode}`);
+    });
+
